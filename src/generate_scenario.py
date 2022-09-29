35a2cdc479ef171c99169da1cacf4a4443514fdd
Christopher Strøm <detoxte@gmail.com>
2022-09-29 16:31:32 +0200

    Add hacky code to visualize multiple timesteps from the tracker

--- src/generate_scenario.py
+++ src/generate_scenario.py
@@ -28,212 +28,226 @@ from modules.models.mt3v2.mt3v2 import MT3V2
 
 from matplotlib.patches import Ellipse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-tp', '--task_params', help='filepath to configuration yaml file defining the task', required=True)
-parser.add_argument('-mp', '--model_params', help='filepath to configuration yaml file defining the model', required=True)
-parser.add_argument('--continue_training_from', help='filepath to folder of an experiment to continue training from')
-parser.add_argument('--exp_name', help='Name to give to the results folder')
-args = parser.parse_args()
-print(f'Task configuration file: {args.task_params}')
-print(f'Model configuration file: {args.model_params}')
 
-# Load hyperparameters
-params = load_yaml_into_dotdict(args.task_params)
-params.update(load_yaml_into_dotdict(args.model_params))
-eval_params = load_yaml_into_dotdict(args.task_params)
-eval_params.update(load_yaml_into_dotdict(args.model_params))
-eval_params.recursive_update(load_yaml_into_dotdict('configs/eval/default.yaml'))
-# Generate 32-bit random seed, or use user-specified one
-random_data = os.urandom(4)
-params.general.pytorch_and_numpy_seed = int.from_bytes(random_data, byteorder="big")
-print(f'Using seed: {params.general.pytorch_and_numpy_seed}')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-tp', '--task_params', help='filepath to configuration yaml file defining the task', required=True)
+    parser.add_argument('-mp', '--model_params', help='filepath to configuration yaml file defining the model', required=True)
+    parser.add_argument('--continue_training_from', help='filepath to folder of an experiment to continue training from')
+    parser.add_argument('--exp_name', help='Name to give to the results folder')
+    args = parser.parse_args()
+    print(f'Task configuration file: {args.task_params}')
+    print(f'Model configuration file: {args.model_params}')
 
-# Seed pytorch and numpy for reproducibility
-torch.manual_seed(params.general.pytorch_and_numpy_seed)
-torch.cuda.manual_seed_all(params.general.pytorch_and_numpy_seed)
-np.random.seed(params.general.pytorch_and_numpy_seed)
+    # Load hyperparameters
+    params = load_yaml_into_dotdict(args.task_params)
+    params.update(load_yaml_into_dotdict(args.model_params))
+    eval_params = load_yaml_into_dotdict(args.task_params)
+    eval_params.update(load_yaml_into_dotdict(args.model_params))
+    eval_params.recursive_update(load_yaml_into_dotdict('configs/eval/default.yaml'))
+    # Generate 32-bit random seed, or use user-specified one
+    random_data = os.urandom(4)
+    params.general.pytorch_and_numpy_seed = int.from_bytes(random_data, byteorder="big")
+    print(f'Using seed: {params.general.pytorch_and_numpy_seed}')
 
-if params.training.device == 'auto':
-    params.training.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-if eval_params.training.device == 'auto':
-    eval_params.training.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # Seed pytorch and numpy for reproducibility
+    torch.manual_seed(params.general.pytorch_and_numpy_seed)
+    torch.cuda.manual_seed_all(params.general.pytorch_and_numpy_seed)
+    np.random.seed(params.general.pytorch_and_numpy_seed)
 
-
-params.data_generation.seed = params.general.pytorch_and_numpy_seed #random.randint(0, 9999999999999999999) # Tune this to get different runs
-do_plot_preds = True
-do_plot_ellipse = True
-do_plot_vel = True
-do_plot_missed_predictions = True
-
-@torch.no_grad()
-def output_truth_plot(ax, prediction, labels, matched_idx, batch, params, training_example_to_plot=0):
-
-    assert hasattr(prediction, 'positions'), 'Positions should have been predicted for plotting.'
-    assert hasattr(prediction, 'logits'), 'Logits should have been predicted for plotting.'
-    if params.data_generation.prediction_target == 'position_and_velocity':
-        assert hasattr(prediction, 'velocities'), 'Velocities should have been predicted for plotting.'
-
-    bs, num_queries = prediction.positions.shape[:2]
-    assert training_example_to_plot <= bs, "'training_example_to_plot' should be less than batch_size"
-
-    if params.data_generation.prediction_target == 'position_and_shape':
-        raise NotImplementedError('Plotting not working yet for shape predictions.')
-
-    # Get ground-truth, predicted state, and logits for chosen training example
-    truth = labels[training_example_to_plot].cpu().numpy()
-    indices = tuple([t.cpu().detach().numpy() for t in matched_idx[training_example_to_plot]])
-    if params.data_generation.prediction_target == 'position':
-        out = prediction.positions[training_example_to_plot].cpu().detach().numpy()
-    elif params.data_generation.prediction_target == 'position_and_velocity':
-        pos = prediction.positions[training_example_to_plot]
-        vel = prediction.velocities[training_example_to_plot]
-        out = torch.cat((pos, vel), dim=1).cpu().detach().numpy()
-    out_prob = prediction.logits[training_example_to_plot].cpu().sigmoid().detach().numpy().flatten()
+    if params.training.device == 'auto':
+        params.training.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if eval_params.training.device == 'auto':
+        eval_params.training.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-    # Optionally get uncertainties for chosen training example
-    if hasattr(prediction, 'uncertainties'):
-        uncertainties = prediction.uncertainties[training_example_to_plot].cpu().detach().numpy()
-    else:
-        uncertainties = None
+    params.data_generation.seed = params.general.pytorch_and_numpy_seed #random.randint(0, 9999999999999999999) # Tune this to get different runs
+    do_plot_preds = True
+    do_plot_ellipse = False
+    do_plot_vel = False
+    do_plot_missed_predictions = False
+
+    @torch.no_grad()
+    def output_truth_plot(ax, prediction, labels, matched_idx, batch, params, training_example_to_plot=0):
+
+        assert hasattr(prediction, 'positions'), 'Positions should have been predicted for plotting.'
+        assert hasattr(prediction, 'logits'), 'Logits should have been predicted for plotting.'
+        if params.data_generation.prediction_target == 'position_and_velocity':
+            assert hasattr(prediction, 'velocities'), 'Velocities should have been predicted for plotting.'
+
+        bs, num_queries = prediction.positions.shape[:2]
+        assert training_example_to_plot <= bs, "'training_example_to_plot' should be less than batch_size"
+
+        if params.data_generation.prediction_target == 'position_and_shape':
+            raise NotImplementedError('Plotting not working yet for shape predictions.')
+
+        # Get ground-truth, predicted state, and logits for chosen training example
+        truth = labels[training_example_to_plot].cpu().numpy()
+        indices = tuple([t.cpu().detach().numpy() for t in matched_idx[training_example_to_plot]])
+        if params.data_generation.prediction_target == 'position':
+            out = prediction.positions[training_example_to_plot].cpu().detach().numpy()
+        elif params.data_generation.prediction_target == 'position_and_velocity':
+            pos = prediction.positions[training_example_to_plot]
+            vel = prediction.velocities[training_example_to_plot]
+            out = torch.cat((pos, vel), dim=1).cpu().detach().numpy()
+        out_prob = prediction.logits[training_example_to_plot].cpu().sigmoid().detach().numpy().flatten()
+
+
+        # Optionally get uncertainties for chosen training example
+        if hasattr(prediction, 'uncertainties'):
+            uncertainties = prediction.uncertainties[training_example_to_plot].cpu().detach().numpy()
+        else:
+            uncertainties = None
 
 
 
 
-    # Plot xy position of measurements, alpha-coded by time
-    measurements = batch.tensors[training_example_to_plot][~batch.mask[training_example_to_plot]]
-    colors = np.zeros((measurements.shape[0], 4))
-    unique_time_values = np.array(sorted(list(set(measurements[:, 3].tolist()))))
-    def f(t):
-        """Exponential decay for alpha in time"""
-        idx = (np.abs(unique_time_values - t)).argmin()
-        return 1/1.2**(len(unique_time_values)-idx)
+        # Plot xy position of measurements, alpha-coded by time
+        measurements = batch.tensors[training_example_to_plot][~batch.mask[training_example_to_plot]]
+        colors = np.zeros((measurements.shape[0], 4))
+        unique_time_values = np.array(sorted(list(set(measurements[:, 3].tolist()))))
+        def f(t):
+            """Exponential decay for alpha in time"""
+            idx = (np.abs(unique_time_values - t)).argmin()
+            return 1/1.2**(len(unique_time_values)-idx)
 
-    colors[:, 3] = [f(t) for t in measurements[:, 3].tolist()]
-    
-    measurements_cpu = measurements.cpu()
-    meas_r = measurements_cpu[:, 0]
-    meas_theta = measurements_cpu[:, 2]
-    meas_x = meas_r * np.cos(meas_theta)
-    meas_y = meas_r * np.sin(meas_theta)
+        colors[:, 3] = [f(t) for t in measurements[:, 3].tolist()]
+        
+        measurements_cpu = measurements.cpu()
+        meas_r = measurements_cpu[:, 0]
+        meas_theta = measurements_cpu[:, 2]
+        meas_x = meas_r * np.cos(meas_theta)
+        meas_y = meas_r * np.sin(meas_theta)
 
-    ax.scatter(meas_x, meas_y, marker='x', c=colors, zorder=np.inf)
-    # ax.scatter(X, Y, marker='x', color="k", alpha=0.8, label="True Measurements")
-    # ax.scatter(Xf, Yf, marker='x', color="r", alpha=0.8, label="False Measurements")
+        if params.data_generation.n_prediction_lag == 0:
 
-    once = True
-    for i, posvel in enumerate(out):
-        pos_x = posvel[0]
-        pos_y = posvel[1]
-        vel_x = posvel[2]
-        vel_y = posvel[3]
+            ax.scatter(meas_x, meas_y, marker='x', c=colors, zorder=np.inf)
+            # ax.scatter(X, Y, marker='x', color="k", alpha=0.8, label="True Measurements")
+            # ax.scatter(Xf, Yf, marker='x', color="r", alpha=0.8, label="False Measurements")
 
-        if i in indices[0]:
+        once = True
+        for i, posvel in enumerate(out):
+            pos_x = posvel[0]
+            pos_y = posvel[1]
+            vel_x = posvel[2]
+            vel_y = posvel[3]
 
-            gt_idx = indices[1][np.where(indices[training_example_to_plot] == i)[0][0]]
-            gt_x = truth[gt_idx][0]
-            gt_y = truth[gt_idx][1]
-            gt_vx = truth[gt_idx][2]
-            gt_vy = truth[gt_idx][3]
+            if i in indices[0]:
 
-            if do_plot_preds:
-                # Plot predicted positions
-                p = ax.plot(pos_x, pos_y, marker='o', label='Object Pred', markersize=5)
-                color = p[0].get_color()
+                gt_idx = indices[1][np.where(indices[training_example_to_plot] == i)[0][0]]
+                gt_x = truth[gt_idx][0]
+                gt_y = truth[gt_idx][1]
+                gt_vx = truth[gt_idx][2]
+                gt_vy = truth[gt_idx][3]
 
-                # Plot ground-truth
-                ax.plot(gt_x, gt_y, marker='D', color=color, label='Object GT', markersize=5)
+                if do_plot_preds:
+                    # Plot predicted positions
+                    p = ax.plot(pos_x, pos_y, marker='o', color='r', label='Object Pred', markersize=5)
+                    color = p[0].get_color()
 
-            # Plot arrows that indicate velocities for each object
-            if params.data_generation.prediction_target == 'position_and_velocity' and do_plot_vel:
-                ax.arrow(pos_x, pos_y, vel_x, vel_y, color=color, head_width=0.2, linestyle='--', length_includes_head=True)
-                ax.arrow(gt_x, gt_y, gt_vx, gt_vy, color=p[0].get_color(), head_width=0.2, length_includes_head=True)
+                    # Plot ground-truth
+                    ax.plot(gt_x, gt_y, marker='D', color='g', label='Object GT', markersize=5)
 
-            # Plot uncertainties (2-sigma ellipse)
-            if uncertainties is not None and do_plot_ellipse:
-                ell_position = Ellipse(xy=(pos_x, pos_y), width=uncertainties[i, 0]*4, height=uncertainties[i, 1]*4,
-                                       color=color, alpha=0.4)
-                ell_velocity = Ellipse(xy=(indices
-                else:
-                    p = ax.plot(pos_x, pos_y, marker='*', color='k', markersize=5)
-
+                # Plot arrows that indicate velocities for each object
                 if params.data_generation.prediction_target == 'position_and_velocity' and do_plot_vel:
-                    ax.arrow(pos_x, pos_y, vel_x, vel_y, color='k', head_width=0.2, linestyle='--', length_includes_head=True)
+                    ax.arrow(pos_x, pos_y, vel_x, vel_y, color=color, head_width=0.2, linestyle='--', length_includes_head=True)
+                    ax.arrow(gt_x, gt_y, gt_vx, gt_vy, color=p[0].get_color(), head_width=0.2, length_includes_head=True)
+
+                # Plot uncertainties (2-sigma ellipse)
+                if uncertainties is not None and do_plot_ellipse:
+                    ell_position = Ellipse(xy=(pos_x, pos_y), width=uncertainties[i, 0]*4, height=uncertainties[i, 1]*4,
+                                        color=color, alpha=0.4)
+                    ell_velocity = Ellipse(xy=(pos_x + vel_x, pos_y + vel_y), width=uncertainties[i, 2]*4,
+                                        height=uncertainties[i, 3]*4, edgecolor=color, linestyle='--', facecolor='none')
+                    ax.add_patch(ell_position)
+                    ax.add_patch(ell_velocity)
+            else:
+                if do_plot_missed_predictions:
+                    if once:
+                        p = ax.plot(pos_x, pos_y, marker='*', color='k', label='Unmatched Predicted Object', markersize=5)
+                        once = False
+                    else:
+                        p = ax.plot(pos_x, pos_y, marker='*', color='k', markersize=5)
+
+                    if params.data_generation.prediction_target == 'position_and_velocity' and do_plot_vel:
+                        ax.arrow(pos_x, pos_y, vel_x, vel_y, color='k', head_width=0.2, linestyle='--', length_includes_head=True)
+
+        #     label = "{:.2f}".format(out_prob[i])
+        #     ax.annotate(label, # this is the text
+        #                 (out[i,0], out[i,1]), # this is the point to label
+        #                 textcoords="offset points", # how to position the text
+        #                 xytext=(0,10), # distance from text to points (x,y)
+        #                 ha='center',
+        #                 color=p[0].get_color())
+
+            #ax.legend()
 
 
-    #     label = "{:.2f}".format(out_prob[i])
-    #     ax.annotate(label, # this is the text
-    #                 (out[i,0], out[i,1]), # this is the point to label
-    #                 textcoords="offset points", # how to position the text
-    #                 xytext=(0,10), # distance from text to points (x,y)
-    #                 ha='center',
-    #                 color=p[0].get_color())
+    data_generator = DataGenerator(params)
+    last_filename = "C:/Users/chris/MT3v2/task1/checkpoints/" + "checkpoint_gradient_step_999999"
 
-        ax.legend()
+    # Load model weights and pass model to correct device
+    model = MT3V2(params)
 
+    checkpoint = torch.load(last_filename, map_location=params.training.device)
+    model.load_state_dict(checkpoint['model_state_dict'])
 
-data_generator = DataGenerator(params)
-last_filename = "/home/strom/models/task1/checkpoints/" + "checkpoint_gradient_step_999999"
+    model.to(torch.device(params.training.device))
 
-# Load model weights and pass model to correct device
-model = MT3V2(params)
-
-checkpoint = torch.load(last_filename, map_location=params.training.device)
-model.load_state_dict(checkpoint['model_state_dict'])
-
-model.to(torch.device(params.training.device))
-
-mot_loss = MotLoss(params)
-mot_loss.to(params.training.device)
+    mot_loss = MotLoss(params)
+    mot_loss.to(params.training.device)
 
 
 
-# Calculate all values used for plotting
-batch, labels, unique_ids, _, trajectories, true_measurements, false_measurements = data_generator.get_batch()
+    # Calculate all values used for plotting
+    batch, labels, unique_ids, _, trajectories, true_measurements, false_measurements = data_generator.get_batch()
+    
+    gt = true_measurements[0]
+    ft = false_measurements[0]
+    #print(gt_one)
 
-gt = true_measurements[0]
-ft = false_measurements[0]
-#print(gt_one)
-
-X = []
-Y = []
-#for gt in true_measurements:
-for sample in gt:
-    h = sample[3]
-    r = sample[0]
-    theta = sample[2]
-    X.append(r * np.cos(theta))
-    Y.append(r * np.sin(theta))
-
-
-Xf= []
-Yf = []
-for sample in ft:
-    h = sample[3]
-    r = sample[0]
-    theta = sample[2]
-    Xf.append(r * np.cos(theta))
-    Yf.append(r * np.sin(theta))
-
-prediction, intermediate_predictions, encoder_prediction, aux_classifications, _ = model.forward(batch)
-loss_dict, indices = mot_loss.forward(labels, prediction, intermediate_predictions, encoder_prediction, loss_type=params.loss.type)
-
-fig = plt.figure(constrained_layout=True)
-
-gs = GridSpec(2, 2, figure=fig)
-
-output_ax = fig.add_subplot(111)
-output_ax.set_ylabel('Y')
-output_ax.set_xlabel('X')
-output_ax.set_aspect('equal', 'box')
+    X = []
+    Y = []
+    #for gt in true_measurements:
+    for sample in gt:
+        h = sample[3]
+        r = sample[0]
+        theta = sample[2]
+        X.append(r * np.cos(theta))
+        Y.append(r * np.sin(theta))
 
 
-output_ax.cla()
-output_ax.grid('on')
-output_truth_plot(output_ax, prediction, labels, indices, batch, params, 0)
+    Xf= []
+    Yf = []
+    for sample in ft:
+        h = sample[3]
+        r = sample[0]
+        theta = sample[2]
+        Xf.append(r * np.cos(theta))
+        Yf.append(r * np.sin(theta))
 
-#output_ax.set_xlim([-params.data_generation.field_of_view.max_range*0.2, params.data_generation.field_of_view.max_range*1.2])
-#output_ax.set_ylim([-params.data_generation.field_of_view.max_range, params.data_generation.field_of_view.max_range])
+    fig = plt.figure(constrained_layout=True)
 
-plt.show()
+    gs = GridSpec(2, 2, figure=fig)
+
+    output_ax = fig.add_subplot(111)
+    output_ax.set_ylabel('Y')
+    output_ax.set_xlabel('X')
+    output_ax.set_aspect('equal', 'box')
+
+
+    output_ax.cla()
+    output_ax.grid('on')
+
+    for i in range(3):
+        params.data_generation.n_prediction_lag = i
+        prediction, intermediate_predictions, encoder_prediction, aux_classifications, _ = model.forward(batch)
+        loss_dict, indices = mot_loss.forward(labels, prediction, intermediate_predictions, encoder_prediction, loss_type=params.loss.type)
+
+        output_truth_plot(output_ax, prediction, labels, indices, batch, params, 0)
+
+    #output_ax.set_xlim([-params.data_generation.field_of_view.max_range*0.2, params.data_generation.field_of_view.max_range*1.2])
+    #output_ax.set_ylim([-params.data_generation.field_of_view.max_range, params.data_generation.field_of_view.max_range])
+
+    plt.show()
 
