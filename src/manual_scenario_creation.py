0fd80778ae2d54125e58aef95e38f40776d309dd
Christopher Strøm <detoxte@gmail.com>
2022-11-07 17:14:16 +0100

    Add csv output generator
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- src/manual_scenario_creation.py
+++ src/manual_scenario_creation.py
@@ -1,14 +1,11 @@
 """
-TODO: Recreate data generator based on constant velocity vectors 
-      Which are euler stepped to generate positions, under the constraint that the 
-      velocity length vector is fixed for all tracks (to ensure collision)
 
-TODO: Generate CSV 
-
+TODO: More realistic track evolution.
 
 """
 
 import numpy as np
+import csv
 
 import matplotlib.pyplot as plt
 
@@ -23,16 +20,23 @@ class CVObject:
     y: float
     vx: float
     vy: float
-    dt: float = 1
+    id: int = None
+    dt: float = 0.1
+    t: float = 0
     
     def __post_init__(self):
-        self.track = np.array([[self.x, self.y]])
+        self.track = np.array(self.current_gt())
+        
+    def current_gt(self):
+        return [[int(self.id), self.x, self.y, self.vx, self.vy, np.round(self.t)]]
         
 
     def step(self):
         self.x += self.vx * self.dt
         self.y += self.vy * self.dt
-        self.track = np.append(self.track, [[self.x, self.y]], axis=0)
+        self.t += self.dt
+
+        self.track = np.append(self.track, self.current_gt(), axis=0)
 
     def scatter(self, decay_opacity=True):
         if decay_opacity:
@@ -40,32 +44,37 @@ class CVObject:
         else:
             alpha = 1
 
-        plt.scatter(self.track[:, 0], self.track[:, 1], alpha=alpha)
+        plt.scatter(self.track[:, 1], self.track[:, 2], alpha=alpha)
 
 
-def plot_lambda(x, f):
-    decaying_opacity = np.linspace(0.25, 1, len(x))
-    plt.scatter(f(x)[0], f(x)[1], alpha=decaying_opacity)
 
-
-def cv_object(r, theta):
+def cv_object(r, theta, id):
     return CVObject(-r * np.cos(theta),
                     -r * np.sin(theta),
-                    np.cos(theta),
-                    np.sin(theta))
+                    5*np.cos(theta),
+                    5*np.sin(theta),
+                    id)
     
 
-# def generate_csv(X, functions):
+
+def generate_csv(objects, n_steps):
+
+    track_sorted_by_time = []
+    for step in range(n_steps):
+        tracks = []
+        
+        for object in objects:
+            tracks.append(object.track[step].tolist())
+        
+        track_sorted_by_time.append(tracks)
+
+    with open("tracks.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        for step in track_sorted_by_time:
+
+            writer.writerows(step)
+        
     
-#     for step in X:
-#         for f in functions:
-#             x = f(X[step])[0]
-#             y = f(X[step])[1]
-#             t = np.round(step * 0.1, 4)
-#             vx = 
-#             vy = 
-
-
 
 if __name__ == "__main__":
 
@@ -77,7 +86,7 @@ if __name__ == "__main__":
 
     objects = []
     for i in range(n_traj):
-        objects.append(cv_object(10, theta - i*beta))
+        objects.append(cv_object(5, theta - i*beta, id=i))
 
     for step in range(n_steps):
         for object in objects:
@@ -93,6 +102,10 @@ if __name__ == "__main__":
     leg = plt.legend(legend)
     for lh in leg.legendHandles: 
         lh.set_alpha(1)
+
+    generate_csv(objects, n_steps)
+
+
         
     plt.title("Manually generated test scenario. Points with greater alpha are more recent.")
     plt.xlim([-11, 11])
